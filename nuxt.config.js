58ad8440c8f4fa5f02f81d5f154d062eb2236e1e
Jack Domleo <jackdomleo7@gmail.com>
2021-09-25 12:54:30 +0100

    Set page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,8 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'NotoriousParrotPosse',
+    title: 'Notorious Parrot Posse',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -26,9 +21,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
