0d72da239f839d65152565f95b05d97488bf881a
Jack Domleo <jackdomleo7@gmail.com>
2021-09-25 12:57:15 +0100

    Added @nuxtjs/robots module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,18 +21,18 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build'
   ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    '@nuxtjs/robots'
   ],
+  robots: {
+    UserAgent: '*',
+    Allow: '/'
+  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
