db0f455ecc4c8ca8760f7eba56c61ae8014c17e2
Jack Domleo <jackdomleo7@gmail.com>
2021-09-25 13:20:04 +0100

    Install SCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,16 +15,14 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/scss/main.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build'
   ],
   modules: [
