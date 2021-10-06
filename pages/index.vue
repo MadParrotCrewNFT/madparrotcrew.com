01914a94d06d53f1f785f0193d82409542b05fcf
Jack Domleo <jackdomleo7@gmail.com>
2021-10-06 12:27:25 +0100

    Refactor SCSS

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="home">
-    <header class="home__header">
+  <div>
+    <header class="header">
       <navigation />
-      <calculator class="home__calculator" />
+      <calculator class="header__calculator" />
     </header>
-    <section id="about" class="home__about">
+    <section id="about" class="about">
       <div>
-        <div class="home__about-text">
+        <div class="about__text">
           <h2>About <span :aria-label="siteconfig.brand_name">NPP</span></h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </div>
@@ -86,17 +86,12 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-header {
+.header {
+  position: relative;
   background-color: skyblue;
   min-height: 36rem;
   height: 90vh;
   max-height: 46rem;
-}
-
-.home {
-  &__header {
-    position: relative;
-  }
 
   &__calculator {
     position: absolute;
@@ -104,22 +99,22 @@ header {
     left: 50%;
     transform: translate(-50%, -50%);
   }
+}
 
-  &__about {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    margin-block: 9rem;
+.about {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin-block: 9rem;
 
-    &-text {
-      max-width: 23rem;
-      margin-inline: auto;
-    }
+  &__text {
+    max-width: 23rem;
+    margin-inline: auto;
+  }
 
-    h2 {
-      font-size: var(--font-size-subtitle);
-      margin-top: 0;
-      margin-bottom: 2.75rem;
-    }
+  h2 {
+    font-size: var(--font-size-subtitle);
+    margin-top: 0;
+    margin-bottom: 2.75rem;
   }
 }
 
