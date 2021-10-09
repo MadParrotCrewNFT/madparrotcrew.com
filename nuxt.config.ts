ad30b31485ac9e81c714cdf77e4a33181910e440
Jack Domleo <jackdomleo7@gmail.com>
2021-10-09 22:33:24 +0100

    Added PWA meta & icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,8 +50,20 @@ export default {
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
+    meta: {
+      ogHost: siteconfig.base_url,
+      ogSiteName: siteconfig.brand_name.full,
+      ogType: 'website',
+      twitterCard: 'summary_large_image'
+    },
     manifest: {
-      lang: 'en'
+      name: siteconfig.brand_name.full,
+      short_name: siteconfig.brand_name.short,
+      /* icons: handled by pwa.icons module, */
+      description: '',
+      start_url: '/',
+      display: 'browser',
+      useWebmanifestExtension: true
     }
   },
 
