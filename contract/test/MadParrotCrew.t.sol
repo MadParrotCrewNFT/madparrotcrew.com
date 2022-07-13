a4a502f8fb1d4d5e43f0656cbcaf818f0c2c159a
FancyRatsDave <dave@fancyrats.io>
2022-07-12 19:35:20 -0500

    Remove unused constant

--- contract/test/MadParrotCrew.t.sol
+++ contract/test/MadParrotCrew.t.sol
@@ -11,8 +11,6 @@ contract ContractTest is Test {
     address minter = 0x46F21698726d22d270040Ea720B2d2E72e054888;
     address teamWallet = 0x443B3af4A1920F492F2090973ae2d0Aa81b892aa;
 
-    uint16 maxAfterPremint = 6969 - 30;
-
     function setUp() public {
         uint256[] memory shares = new uint256[](2);
         shares[0] = 20;
